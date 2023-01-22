fix some bugs in dropout / batch normalization. layer normalization is now ready.

assignments/assignment2/cs231n/classifiers/fc_net.py
@@ -81,14 +81,14 @@ class FullyConnectedNet(object):
         dims.extend(hidden_dims)
         dims.append(num_classes)
 
-        for i in range(self.num_layers-1) :
+        for i in range(self.num_layers) :
           self.params['W{}'.format(i+1)] = rng.normal(loc=0.0, scale=weight_scale, size=(dims[i], dims[i+1]))
-          self.params['b{}'.format(i+1)] = np.zeros(hidden_dims[i])
+          self.params['b{}'.format(i+1)] = np.zeros(dims[i+1])
+        
         if self.normalization == "batchnorm":
-          for i in range(self.num_layers-2) :
-            self.params['gamma{}'.format(i+1)] = np.ones(hidden_dims[i])
-            self.params['beta{}'.format(i+1)] = np.zeros(hidden_dims[i])
-
+          for i in range(len(hidden_dims)) :
+            self.params['gamma{}'.format(i+1)] = np.ones(dims[i+1])
+            self.params['beta{}'.format(i+1)] = np.zeros(dims[i+1])
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
         #                             END OF YOUR CODE                             #
@@ -227,7 +227,7 @@ class FullyConnectedNet(object):
         
         ##########################################################################
         
-        count_max = self.num_layers-1
+        count_max = self.num_layers
         count = count_max 
         scores, caches = repeat(Func, caches, count, count_max, X)      
 
@@ -309,7 +309,7 @@ class FullyConnectedNet(object):
         ###########################################################################
         
         count = 1     
-        count_max = self.num_layers-1
+        count_max = self.num_layers
         dX, grads = reverse(Func, caches, grads, count, count_max, dL)
 
         ###########################################################################

assignments/assignment2/cs231n/layers.py
@@ -288,7 +288,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         #######################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        x1 = x-running_mean / np.sqrt(running_var + eps) #broadcasting
+        x1 = (x - running_mean) / np.sqrt(running_var + eps) #broadcasting
         out = gamma * x1 + beta
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -382,7 +382,7 @@ def batchnorm_backward_alt(dout, cache):
     dbeta = np.sum(dout, axis=0)
     dgamma = np.sum(dout * x1, axis=0)
     
-    dx = (dout - (np.tile(dbeta, (N,1)) + dgamma * x1) / N) * gamma / sigma
+    dx = (dout - (dbeta + dgamma * x1) / N) * gamma / sigma
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -427,7 +427,17 @@ def layernorm_forward(x, gamma, beta, ln_param):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    N, D = x.shape
+    
+    mean = np.sum(x, axis=1, keepdims=True) / D
+    centered_x = x-mean
+    var = np.sum(centered_x*centered_x, axis=1, keepdims=True) / D
+    sigma = np.sqrt(var + eps)
+
+    x1 = centered_x / sigma
+    out = x1 * gamma + beta
+
+    cache = x1, sigma, gamma
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -460,8 +470,18 @@ def layernorm_backward(dout, cache):
     # still apply!                                                            #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    
+    x1, sigma, gamma = cache
+    N, D = dout.shape
+    
+    dgamma = np.sum(dout * x1, axis=0)
+    dbeta = np.sum(dout, axis=0)
+    
+    temp = dout * gamma
+    temp1 = np.sum(temp, axis=1, keepdims=True)
+    temp2 = np.sum(temp * x1, axis=1, keepdims=True)
+    
+    dx = (temp - (temp1 + x1*temp2) / D) / sigma
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -508,9 +528,7 @@ def dropout_forward(x, dropout_param):
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         
         mask = np.random.rand(*x.shape) < p
-        #mask = np.random.choice([1, 0], size=x.shape, p=[p,1-p])
         out = x * mask / p
-        #out = x * mask
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         #######################################################################